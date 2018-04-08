db1ddad014c633c4efacb893267237f2c4f992ed
Unknown <the_flicker7@hotmail.com>
2018-04-08 11:06:36 -0400

    Toggle between pages

--- css-experimentation-app/src/app/app.module.ts
+++ css-experimentation-app/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { SimpleCssComponent } from './simple-css/simple-css.component';
 import { AdvancedCssComponent } from './advanced-css/advanced-css.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleCssComponent,
-    AdvancedCssComponent
+    AdvancedCssComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
